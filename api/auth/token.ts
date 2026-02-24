f22513e3f43de5be93cb63aa743d6d94d050a7e8
SkyNest <haikalzy011@gmail.com>
2026-02-25 03:10:44 +0700

    Change import from orderkuota.js to orderkuota.ts

--- api/auth/token.ts
+++ api/auth/token.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getToken } from '../../lib/orderkuota.js';
+import { getToken } from '../../lib/orderkuota.ts';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'OPTIONS') {
